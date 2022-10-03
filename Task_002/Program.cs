3ed0e7122f9cd5dd78d9e51d216af1763636b8ea
Evgeniy <bridskijevgeny@yandex.ru>
2022-10-04 01:33:08 +0300

    2-ая задача, максимальное из 3-х чисел

---
+++ Task_002/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+double FirstNumber = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+double SecondNumber = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+double ThirdNumber = double.Parse(Console.ReadLine());
+
+if (FirstNumber > SecondNumber & FirstNumber > ThirdNumber)
+{
+    Console.WriteLine("Наибольшее число: " + FirstNumber);
+   
+}
+
+else if (SecondNumber > FirstNumber & SecondNumber > ThirdNumber)
+{
+    Console.WriteLine("Наибольшее число: " + SecondNumber);
+   
+}
+
+else if (ThirdNumber > FirstNumber & ThirdNumber > SecondNumber )
+{
+    Console.WriteLine("Наибольшее число: " + ThirdNumber);
+   
+}
+
+else if (ThirdNumber == FirstNumber & ThirdNumber == SecondNumber )
+{
+    Console.WriteLine("Ошибка! Числа равны");
+   
+}
+
