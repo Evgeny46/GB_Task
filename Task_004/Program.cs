0e09d9afe55f3e970d43c895045a4ad1f17a6324
Evgeniy <bridskijevgeny@yandex.ru>
2022-10-04 01:40:03 +0300

    Задача на четные числа в выбранном диапазоне

---
+++ Task_004/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Введите последнее число диапазона: ");
+int number = int.Parse(Console.ReadLine());
+int count = 1;
+
+while(count <= number)
+{
+    if(count % 2 == 0)
+    {
+        Console.WriteLine($"Число {count} является четным");
+    }
+    count++;
+}
+
