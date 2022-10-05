4ab2ed10117ba9ca90855e914db8e108f59a415d
Evgeniy <bridskijevgeny@yandex.ru>
2022-10-05 21:04:41 +0300

    Задача 2. удаление средней цифры

---
+++ Task007/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. Не использовать строки для расчета.
+
+456 -> 46
+782 -> 72
+918 -> 98 */
+
+Console.Clear();
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+int firstnumber = number / 100;
+int secondnumber = number % 10;
+
+string ab = firstnumber.ToString();
+string dc = secondnumber.ToString();
+
+string FinalNumber = ab+dc; 
+Console.WriteLine("Итоговое число " + FinalNumber);
+
+
