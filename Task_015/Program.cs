3eb30afe015f7517ac4ef7b2952381b3c4831ee2
Evgeniy <bridskijevgeny@yandex.ru>
2022-10-18 22:26:20 +0300

    Создание массива

---
+++ Task_015/Program.cs
@@ -0,0 +1,17 @@
+﻿
+/* Задача 3: Напишите программу, которая задаёт массив из 8 случайных элементов и выводит их на экран.
+1, 2, 5, 7, 19, 6, 7, 8 */
+
+Console.Clear();
+
+
+int[] array = new int[8];
+void PrintArray()
+{
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,100);
+    Console.Write($"{array[i]}, ");
+}
+}
+PrintArray();
