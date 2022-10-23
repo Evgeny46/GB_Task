7f629ae21af09d7e31d293124537fbab41458eaa
Evgeniy <bridskijevgeny@yandex.ru>
2022-10-23 16:20:05 +0300

    Рекусия. Примеры

---
+++ Task_019/Program.cs
@@ -0,0 +1,24 @@
+﻿// Рекурсия
+
+/* Console.Clear();
+double Factorial (int n)
+{
+    if(n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+} */
+
+//Числа Фибоначчи
+
+int Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine($"{i} = {Fibonacci(i)}");
+}
