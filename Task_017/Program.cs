8de172fac2b6565f7e0eecdd070242734a71da7d
Evgeniy <bridskijevgeny@yandex.ru>
2022-10-23 14:31:36 +0300

    Сортировка массива от большего к меньшему

---
+++ Task_017/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.Clear();
+int[] arr = {1, 3, 4, 8, 9, 7, 2, 5, 6};
+
+void PrintArray(int[] array)
+{
+    for( int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int min = i;
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[min]) min = j;
+        }
+        int temporary = array[i];
+        array[i] = array[min];
+        array[min] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
