9c1e029f957a0fcde5e3115707948e5b5f690e3c
Evgeniy <bridskijevgeny@yandex.ru>
2022-10-29 17:38:54 +0300

    Сумма элементов под нечетным индексом

---
+++ Task_022/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Задача 2: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0 */
+
+Console.Clear();
+
+int[] array = new int[8];
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+void FillArray(int[] arr2)
+{
+    for (int i = 0; i < arr2.Length; i++)
+    {
+        arr2[i] = new Random().Next(1,10);
+    }
+}
+
+int NumbersUneven(int[] arr3)
+{
+    int count = 0;
+    int sum = 0;
+    for (int i = 0; i < arr3.Length; i++)
+    {
+        if(i % 2 == 0) count++;
+        else sum = sum + arr3[i]; 
+    }
+    return sum;
+}
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int result = NumbersUneven(array);
+Console.WriteLine("Сумма нечетных элементов массива: " + result);
